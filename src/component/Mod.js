a7d9827889c2e8480c2e43cc0cbd688c9558059b
masudul alam <masudulalam972@gmail.com>
2024-03-17 04:55:13 +0600

    version 1.1.9

--- src/component/Mod.js
+++ src/component/Mod.js
@@ -1,52 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink, useNavigate, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import "./Mod.css";
 import PropTypes from "prop-types";
-import Cards from './Card';
-
+import Cards from "./Card";
 
 export default function Mod(props) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [cardData, setCardData] = useState([]);
   const [filteredCards, setFilteredCards] = useState([]);
   const navigate = useNavigate();
   const location = useLocation();
-  
 
   const handleHomeClick = () => {
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   useEffect(() => {
-    // Fetch data from JSON file
-    fetch('/cardData.json')
-      .then(response => response.json())
-      .then(data => setCardData(data))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("/cardData.json")
+      .then((response) => response.json())
+      .then((data) => setCardData(data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   const handleSearch = (e) => {
-    e.preventDefault(); // Prevent default form submission behavior
-  
-    // If search term is empty, do not proceed
+    e.preventDefault();
+    
     if (!searchTerm.trim()) {
-      // Optionally, you can display a message to the user indicating that the search term is empty
       return;
     }
-  
-    // Filter the cards based on the search term
-    const filtered = cardData.filter(card => card.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    const filtered = cardData.filter((card) =>
+      card.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     setFilteredCards(filtered);
-  
-    // Navigate to the searchfound path only if there are matching cards
+
     if (filtered.length > 0) {
-      navigate('/searchfound');
+      navigate("/searchfound");
     }
   };
 
   const getNavLinkStyle = (route) => {
     return {
-      color: location.pathname === route ? '#D24545' : 'white',
+      color: location.pathname === route ? "#D24545" : "white",
     };
   };
 
@@ -55,7 +50,8 @@ export default function Mod(props) {
       <nav className="navbar  navbar-expand-lg  navbar-dark bg-dark">
         <div className="container-fluid">
           <h4 className="navbar-brand" to="/">
-            {props.title}<span className='head'>{props.title2}</span>
+            {props.title}
+            <span className="head">{props.title2}</span>
           </h4>
           <button
             className="navbar-toggler"
@@ -71,22 +67,46 @@ export default function Mod(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink exact to="/" className="nav-link" activeClassName="active" style={getNavLinkStyle('/')}>
+                <NavLink
+                  exact
+                  to="/"
+                  className="nav-link"
+                  activeClassName="active"
+                  style={getNavLinkStyle("/")}
+                >
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink exact to="/request" className="nav-link" activeClassName="active" style={getNavLinkStyle('/request')}>
+                <NavLink
+                  exact
+                  to="/request"
+                  className="nav-link"
+                  activeClassName="active"
+                  style={getNavLinkStyle("/request")}
+                >
                   Request
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink exact to="/about" className="nav-link" activeClassName="active" style={getNavLinkStyle('/about')}>
+                <NavLink
+                  exact
+                  to="/about"
+                  className="nav-link"
+                  activeClassName="active"
+                  style={getNavLinkStyle("/about")}
+                >
                   About
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink exact to="/login/singup" className="nav-link" activeClassName="active" style={getNavLinkStyle('/login/singup')}>
+                <NavLink
+                  exact
+                  to="/login/singup"
+                  className="nav-link"
+                  activeClassName="active"
+                  style={getNavLinkStyle("/login/singup")}
+                >
                   Log in <i className="user fa-regular fa-user"></i>
                 </NavLink>
               </li>
@@ -98,7 +118,7 @@ export default function Mod(props) {
                 placeholder="Search"
                 aria-label="Search"
                 value={searchTerm}
-                onChange={e => setSearchTerm(e.target.value)}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
               <button
                 className="btn btn-outline-success"
@@ -112,12 +132,33 @@ export default function Mod(props) {
         </div>
       </nav>
       <br />
-      {/* Conditionally render the Cards component or a message */}
-      {location.pathname === '/searchfound' && filteredCards.length > 0 && (
+      {location.pathname === "/searchfound" && filteredCards.length > 0 && (
         <Cards cardData={filteredCards} />
       )}
-      {location.pathname === '/searchfound' && filteredCards.length === 0 && (
-        <div className="message" style={{color:"#D24545",fontSize:"1.5rem",textAlign:"center",padding:"5px",backgroundColor:"white"}}>No match found go  <button onClick={handleHomeClick} style={{border:"none",background:"none",color:"red",textDecoration:"underline"}}>home</button> </div>
+      {location.pathname === "/searchfound" && filteredCards.length === 0 && (
+        <div
+          className="message"
+          style={{
+            color: "#D24545",
+            fontSize: "1.5rem",
+            textAlign: "center",
+            padding: "5px",
+            backgroundColor: "white",
+          }}
+        >
+          No match found go{" "}
+          <button
+            onClick={handleHomeClick}
+            style={{
+              border: "none",
+              background: "none",
+              color: "red",
+              textDecoration: "underline",
+            }}
+          >
+            home
+          </button>{" "}
+        </div>
       )}
     </div>
   );
